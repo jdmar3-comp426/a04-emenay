cca859f73d7e93e54038924b9bb7fb8570f005ac
emenay <57572123+emenay@users.noreply.github.com>
2021-11-30 20:11:59 -0500

    8

--- server.js
+++ server.js
@@ -39,7 +39,8 @@ app.get("/app/users", (req, res) => {
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
 app.get("/app/users:id", (req, res) => {	
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?").all();
-	res.status(200).json(stmt.get(req.params.id));
+	const out = stmt.get(req.params.id);
+	res.status(200).json(out);
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
