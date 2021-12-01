fbc768ff1528875466ed71a291b764312db92157
emenay <57572123+emenay@users.noreply.github.com>
2021-11-30 19:59:16 -0500

    7

--- server.js
+++ server.js
@@ -43,6 +43,10 @@ app.get("/app/users:id", (req, res) => {
 });
 
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
+app.patch("/app/update/user/:id", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").all();
+	res.status(200).json(stmt.get(req.params.id));
+});
 
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 
