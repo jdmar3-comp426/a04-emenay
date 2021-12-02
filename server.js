9db4736e2be49bd47d41f347f60faaadc1f4f5a3
emenay <57572123+emenay@users.noreply.github.com>
2021-12-02 14:26:11 -0500

    15

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 // Define app using express
 var express = require("express")
 var app = express()
-    // Require database SCRIPT file
-var db = require("./database.js")
+// Require database SCRIPT file
+var db = require("./database.js");
 
 // Require md5 MODULE
-var md5 = require("md5")
+var md5 = require("md5");
+
+// Require a middleware extension for express
+var bodyParser = require("body-parser");
+const { unsafeMode } = require("./database.js");
 
 // Make Express use its own built-in body parser
 app.use(express.urlencoded({ extended: true }));
@@ -13,57 +17,56 @@ app.use(express.json());
 
 // Set server port
 HTTP_PORT = 5000
-    // Start server
+
+// Start server
 app.listen(HTTP_PORT, () => {
-    // console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT))
+    //console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({ "message": "Your API works! (200)" });
-    res.status(200);
+    res.json({"message":"Your API works! (200)"});
+	res.status(200);
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.post("/app/new", (req, res) => {
-    const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-    const info = stmt.run(req.body.user, md5(req.body.pass))
-        //{"message":"1 record created: ID 3 (201)"}{"id":3,"user":"newtest","pass":"38a7744f5523335db845ff1976bf4747"}%
-    res.status(201).send({ message: info.changes + " record created: ID " + info.lastInsertRowid + " (201)" });
-});
-// READ a list of all users (HTTP method GET) at endpoint /app/users/     DONE 
-app.get("/app/users", (req, res) => {
-    const stmt = db.prepare("SELECT * FROM userinfo").all();
-    res.status(200).json(stmt);
+app.post("/app/new/user", (req, res) => {
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?").all();
+	res.status(200).json(stmt.get(req.params.id));
 });
 
-// READ a single user (HTTP method GET) at endpoint /app/user/:id      DONE 
-app.get("/app/user/:id", (req, res) => {
-    const stmt = db.prepare("SELECT user, pass FROM userinfo WHERE id = ?");
-    const info = stmt.get(req.params.id);
-    //const pass = stmt.get(req.body.pass);
-    //pass = md5(pass);
-    var object = {
-        id: parseInt(req.params.id),
-        user: info["user"],
-        pass: info["pass"],
-    }
-    res.status(200).json(object);
+// READ a list of all users (HTTP method GET) at endpoint /app/users/
+app.get("/app/users", (req, res) => {	
+	const stmt = db.prepare("SELECT * FROM userinfo").all();
+	res.status(200).json(stmt);
 });
+
+// READ a single user (HTTP method GET) at endpoint /app/user/:id
+app.get("/app/users:id", (req, res) => {	
+	const stmt = db.prepare("SELECT pass, user FROM userinfo WHERE id = ?");
+	const i = stmt.get(req.params.id)
+	var v = {id: parseInt(req.params.id), user: i["user"], pass: i["pass"]}
+	res.status(200).json(v)
+});
+
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
 app.patch("/app/update/user/:id", (req, res) => {
-    const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-    const info = stmt.run(req.body.user, md5(req.body.pass), req.params.id)
-    res.status(200).send({ message: info.changes + " record updated: ID " + req.params.id + " (200)" });
+	const stmt = db.prepare("UPDATE userinfo SET user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
+	const i = stmt.run(req.body.user, md5(req.body.pass), req.params.id)
+	var v = {message:i.changes+" record updated: ID " + req.params.id+" (200)"}
+	res.status(200).json(stmt.get(req.params.id));
 });
+
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
 app.delete("/app/delete/user/:id", (req, res) => {
-    const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
-    const info = stmt.run(req.params.id)
-    res.status(200).send({ message: info.changes + " record deleted: ID " + req.params.id + " (200)" });
+	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
+	const fuck = stmt.run(req.params.id)
+	res.status(200).send({message: info.changes + " record deleted: ID " + req.params.id + " (200)"});
 });
+
 // Default response for any other request
-app.use(function(req, res) {
-    res.json({ "message": "Your API is working!)" });
+app.use(function(req, res){
+	//res.json({"message":"Endpoint not found. (404)"});
+	res.json({"message":"Your API is working!"});
     res.status(404);
 });
